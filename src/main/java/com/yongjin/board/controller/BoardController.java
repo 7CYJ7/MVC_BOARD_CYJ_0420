6164e40da03e3b85a58d696b671bbe352aad66b5
7CYJ <jojo3528@gmail.net>
2023-04-20 14:35:30 +0900

    글 수정 기능 추가

--- src/main/java/com/yongjin/board/controller/BoardController.java
+++ src/main/java/com/yongjin/board/controller/BoardController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.yongjin.board.command.BContentCommand;
 import com.yongjin.board.command.BListCommand;
+import com.yongjin.board.command.BModifyCommand;
 import com.yongjin.board.command.BWriteCommand;
 
 
@@ -38,6 +40,40 @@ public class BoardController {
 		command.execute(model);
 		
 		return "list";
+	}
+	
+	@RequestMapping(value = "content_view")
+	public String content_view(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("requset", request);
+		
+		BContentCommand command = new BContentCommand();
+		command.execute(model);
+		
+		return "contentView";
+	}
+	
+	@RequestMapping(value="/modify.form")
+	public String modify_form(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("requset", request);
+		
+		BContentCommand command = new BContentCommand();
+		command.execute(model);
+		
+		
+		return "modifyForm";
+	}
+	
+	@RequestMapping(value = "/modify")
+	public String modify(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("request", request);
+		
+		BModifyCommand command = new BModifyCommand();
+		command.execute(model);
+		
+		return "redirect:list";
 	}
 
 }
